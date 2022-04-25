9d696d6541933cd42fe9693806ced7189b563100
jbairoso <jbairoso@gmail.com>
2022-04-24 21:49:56 -0700

    save to local storage feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,60 @@
-//today's date and time
-var formatDate = moment().format('MM Do YYYY, h:mm a');
-$('#currentDay').text(formatDate);
-var noteBlock=document.getElementById("taskDescription");
+let currentDay=$("#currentDay");
+currentDay.text(moment().format("dddd,MMMM Do YYYY"));
+let timeBlocks=$(".time-block");
+let savetextArea={};
 
-
-
-
-for (let i=0; i<saveButtonElements.length; i++) {
-    let button= saveButtonElements[i];
-    button.addEventListener ("click", function(e) {
-        var clickTextContext = button.parentElement.previousElementSibling.closest(".noteDescription").textContext;
-        var timeBlockId= button.parentElement.firstElementChild.getAttribute("id").split("-")[1];
-        localStorage.setItem(timeBlockId, clickTextContext);
-    });
-}
-
-function getCurrentTime() {
-    currentTime=parseInt(currentHour);
-    currentTime=timeNow;
-}
-
-//save button
-$(".saveBtn").on("click", function() {
-    console.log($(this));
-});
-
-for (let i = 0; i< hour.length; i++) {
-    if (currentHour===hour[i]){
-        document.getElementsByClassName("hour"+[i]).classList.add('present');
+//for present past and future tasks
+let formatTimeBlocks=function(){
+    let currentHour=moment().format();
+    let currentHourFound= false;
+    for(let i=0; i<timeBlocks.length;[i]){
+        if (timeBlocks.eq(i).find(".hour").text()===currentHour){
+            timeBlocks.eq(i).find(".event").addClass("present");
+            currentHourFound=true;
+        }
+        else if (currentHourFound){
+            timeBlocks.eq(i).find(".event").addClass("future");
+        }
+        else {
+            timeBlocks.eq(i).find(".event").addClass("past");
+        }
     }
 }
 
-let timeBlockNine=document.getElementById('textArea-9');
-timeBlockNine.textContent = localStorage.getItem("9");
+//data will be saved in local storage 
+function saveTasksToLocalStorage(){
+    for (var i=0; i<=noteBlocks.length; i++){
+        if (noteBlocks[i] !==null){
 
-let timeBlockTen=document.getElementById('textArea-10');
-timeBlockTen.textContent = localStorage.getItem("10")
+        }
+    }
+}
 
-let timeBlockEleven=document.getElementById('textArea-11');
-timeBlockEleven.textContent = localStorage.getItem("11")
+let timeBlockNine = document.getElementById('textArea-9');
+timeBlockNine.textContent=localStorage.getItem("9");
 
-let timeBlockTweleve=document.getElementById('textArea-12');
-timeBlockTweleve.textContent = localStorage.getItem("12")
+let timeBlockTen = document.getElementById('textArea-10');
+timeBlockTen.textContent=localStorage.getItem("10");
 
-let timeBlockThirtteen=document.getElementById('textArea-13');
-timeBlockThirtteen.textContent = localStorage.getItem("13")
+let timeBlockEleven = document.getElementById('textArea-11');
+timeBlockEleven.textContent=localStorage.getItem("11");
 
-let timeBlockFourteen=document.getElementById('textArea-14');
-timeBlockFourteen.textContent = localStorage.getItem("14")
+let timeBlockTwelve = document.getElementById('textArea-12');
+timeBlockTwelve.textContent=localStorage.getItem("12");
 
-let timeBlockFifteen=document.getElementById('textArea-15');
-timeBlockFifteen.textContent = localStorage.getItem("15")
+let timeBlockThirteen = document.getElementById('textArea-13');
+timeBlockThirteen.textContent=localStorage.getItem("13");
 
-let timeBlockSixteen=document.getElementById('textArea-16');
-timeBlockSixteen.textContent = localStorage.getItem("16")
+let timeBlockFourteen = document.getElementById('textArea-14');
+timeBlockFourteen.textContent=localStorage.getItem("14");
 
-let timeBlockSeventeen=getElementById('textArea-17');
-timeBlockSeventeen.textContent = localStorage.getItem("17")
+let timeBlockFifteen = document.getElementById('textArea-15');
+timeBlockFifteen.textContent=localStorage.getItem("15");
+
+let timeBlockSixteen = document.getElementById('textArea-16');
+timeBlockSixteen.textContent=localStorage.getItem("16");
+
+let timeBlockSeventeen = document.getElementById('textArea-17');
+timeBlockSeventeen.textContent=localStorage.getItem("17");
+
+saveTasksToLocalStorage();
